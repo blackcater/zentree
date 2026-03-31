1d9eddcced591fa7bc1a56e6162762e28b189668
Elon Tang <i@blackcater.dev>
2026-03-31 18:00:43 +0800

    feat(desktop): integrate all sidebar sections into AppSidebar

--- apps/desktop/src/renderer/src/components/app-shell/AppSidebar.tsx
+++ apps/desktop/src/renderer/src/components/app-shell/AppSidebar.tsx
@@ -1,16 +1,25 @@
+import { useAtomValue } from 'jotai'
 import { ScrollArea } from '@acme-ai/ui/foundation'
 
 import { SidebarFooter } from './sidebar/SidebarFooter'
 import { SidebarHeader } from './sidebar/SidebarHeader'
+import { PinnedSection } from './sidebar/PinnedSection'
+import { FolderView } from './sidebar/FolderView'
+import { FlatView } from './sidebar/FlatView'
+import { viewModeAtom } from './atoms/thread-atoms'
 
 export function AppSidebar(): React.JSX.Element {
+	const viewMode = useAtomValue(viewModeAtom)
+
 	return (
 		<aside className="text-secondary-foreground relative flex w-[256px] shrink-0 flex-col">
 			<SidebarHeader />
 			{/* Sidebar Content */}
 			<ScrollArea className="flex-1">
-				{/* TODO: 一个 TitleCell */}
-				{/* TODO: 一个 FolderCell + 多个 ThreadCell */}
+				{/* Pinned Section */}
+				<PinnedSection />
+				{/* Thread Section - switches between FolderView and FlatView */}
+				{viewMode === 'folder' ? <FolderView /> : <FlatView />}
 			</ScrollArea>
 			{/* Sidebar Footer */}
 			<SidebarFooter />
